334e4a6c62bc49f6a91b51ec8812b972f31fecb6
Jaume Singla Valls <singlavalls@mediamarktsaturn.com>
2023-05-23 20:16:59 +0200

    Seems the big magic is not working... to automatically call to on_init and on_destroy and this stuff...

--- src/module.v
+++ src/module.v
@@ -1,22 +1,59 @@
 module vest
 
-import arrays {index_of_first}
+import arrays { index_of_first }
+import v.reflection
+
+type InjectCb = fn () !
+
+pub interface InitService {
+	on_init() !
+}
+
+/*
+pub interface DestroyService {
+	on_destroy()!
+}
+*/
 
 pub struct Service {
-	name string
-	instance voidptr
+	typ      int      [required]
+	instance voidptr  [required]
+	inject   InjectCb [required]
+}
+
+fn (self &Service) init() ! {
+	if type_info := reflection.get_type(self.typ) {
+		println(type_info)
+		on_init_idx := index_of_first(type_info.sym.methods, fn (_idx int, method reflection.Function) bool {
+			return method.name == 'on_init' && method.args.len == 1
+		})
+		if on_init_idx != -1 {
+			service := unsafe { InitService(self.instance) }
+			println('on_init')
+			println(service)
+			println(self.instance)
+			service.on_init()!
+			println('Something really bad!')
+		}
+	}
 }
 
 pub struct Module {
 mut:
-	services map[string]Service = map[string]Service{}
+	aliases  map[string]int  = map[string]int{}
+	services map[int]Service = map[int]Service{}
 }
 
-const inject_key = 'inject:'
+const (
+	inject_key = 'inject:'
+	provide_key = 'provide='
+)
 
-fn get_key(attrs []string)?string {
-	first_inject_index := index_of_first(attrs,fn (idx int, attr string) bool{ return attr.starts_with(inject_key)})
-	if first_inject_index>=0{
+fn get_key(attrs []string) ?string {
+	first_inject_index := index_of_first(attrs, fn (idx int, attr string) bool {
+		return attr.starts_with(inject_key)
+	})
+	if first_inject_index >= 0 {
 		attr := attrs[first_inject_index]
 		name := attr[inject_key.len..]
 		return name.trim(' ')
@@ -24,36 +61,92 @@ fn get_key(attrs []string)?string {
 	return none
 }
 
-pub fn (mut self Module) register[T]() !&T {
-	mut new_service := &T{}
-	inject_name := T.name
-
-	$for field in T.fields {
-		if field_inject_name := get_key(field.attrs) {
-			service := self.get_service(field_inject_name) or { return error('Invalid injection name ${field_inject_name} for field ${field.name} in ${T.name}')}
-			new_service.$(field.name)= unsafe {service.instance}
+fn get_aliases[T]() []string {
+	mut aliases_list := [T.name[1..]]
+	if typ := reflection.get_type(typeof[T]().idx) {
+		info := typ.sym.info
+		if info is reflection.Struct {
+			for attr in info.attrs.filter(fn (attr string) bool {
+					return attr.starts_with(provide_key)
+				}) {
+				aliases_list << attr[provide_key.len..]
+			}
 		}
 	}
 
-	if inject_name in self.services {
-		return error('Name ${inject_name} has been already registered')
+	return aliases_list
+}
+
+pub fn (mut self Module) register[T]() &T {
+	mut new_service := &T{}
+	typ_idx := typeof[T]().idx
+	typ := reflection.get_type(typ_idx) or {panic('Type not found ${T.name}')}
+
+	if typ_idx in self.services {
+		panic('Type ${typ.name} has been already registered')
 	}
-	self.services[inject_name] = Service{
-		name: T.name,
+
+	for alias in get_aliases[T]() {
+		self.aliases[alias] = typ_idx
+	}
+	self.services[typ_idx] = Service{
+		//name: T.name
+		typ: typ_idx
 		instance: new_service
+		inject: fn [self, mut new_service] [T]() ! {
+			$for field in T.fields {
+				if field_inject_name := get_key(field.attrs) {
+					service := self.get_service_by_name(field_inject_name) or {
+						return error('Invalid injection name ${field_inject_name} for field ${field.name} in ${T.name}')
+					}
+					new_service.$(field.name) = unsafe { service.instance }
+				} else if 'inject' in field.attrs {
+					service := self.get_service(field.typ) or {
+						return error('Invalid injection type for field ${field.name} in ${T.name}')
+					}
+					new_service.$(field.name) = unsafe { service.instance }
+				}
+			}
+		}
 	}
 	return new_service
 }
 
-fn (self &Module) get_service(name string) !Service {
-	return self.services[name] or { return error('Service with name ${name} not available, see available: ${self.services.keys()}')}
+pub fn (self &Module) inject()! {
+	for typ in self.services.keys() {
+		self.services[typ].inject()!
+	}
 }
 
-pub fn (self &Module) get[T](name string) !&T {
-	service := self.get_service(name)!
-	if service.name == T.name {
-		return unsafe{ &T(service.instance) }
+pub fn (self &Module) init()! {
+	for typ in self.services.keys() {
+		println(typ)
+		println('init')
+		self.services[typ].init()!
+	}
+}
+
+fn (self &Module) get_service(service_idx int) !Service {
+	return self.services[service_idx] or {
+		service_info := reflection.get_type(service_idx) or {
+			return err
+		}
+		return error('Service with name ${service_info.name} not available, see available: ${self.services.keys()}')
+	}
+}
+
+
+fn (self &Module) get_service_by_name(name string) !Service {
+	service_idx := self.aliases[name] or {return error('Service with name ${name} not available, see available: ${self.aliases.keys()}')}
+	return self.get_service(service_idx)
+}
+
+pub fn (self &Module) get[T](name ?string) !&T {
+	lookup_name := name or { T.name[1..] }
+	service := self.get_service_by_name(lookup_name)!
+	if service.typ == typeof[T]().idx {
+		return unsafe { &T(service.instance) }
 	} else {
-		return error('Type ${T.name} is not ${service.name}')
+		return error('Type ${T.name} is not valid')
 	}
 }
--- src/module_test.v
+++ src/module_test.v
@@ -1,37 +1,67 @@
 module vest
 
-struct SimpleService {
+[provide: Name]
+struct SimpleService{
 pub mut:
 	count int
+}
+
+pub fn (mut self SimpleService) on_init()! {
+	println('On init!')
+	self.count = 33
 }
 
 
 
 struct SomeServiceWithInjection {
 pub:
-	serv &SimpleService ['inject: .SimpleService']
+	serv &SimpleService [inject]
+	custom_name &SimpleService [inject: Name]
 }
 
-fn set_up_module() !Module {
+fn set_up_module() Module {
 	mut mod := Module{}
-	service := mod.register[SimpleService]() or {panic(err)}
+	service := mod.register[SimpleService]()
 	return mod
 }
 
-fn test_basic_registration(){
+fn te_st_basic_registration()!{
 	mut mod := Module{}
-	service := mod.register[SimpleService]() or {panic(err)}
+	service := mod.register[SimpleService]()
 
-	service_2 := mod.get[SimpleService]('.SimpleService') or { panic(err)}
+	service_2 := mod.get[SimpleService](none) or { panic(err)}
 	assert service == service_2
 }
 
-fn test_service_with_injection(){
-	mut mod := set_up_module()!
+fn te_st_service_with_injection()!{
+	mut mod := set_up_module()
 
-	subject := mod.register[SomeServiceWithInjection]()!
+	subject := mod.register[SomeServiceWithInjection]()
+	mod.inject() or { panic(err)}
 
-	mod.get[SimpleService]('.SimpleService')!.count = 42
+	mod.get[SimpleService]('SimpleService')!.count = 42
 
 	assert subject.serv.count == 42
+}
+
+fn test_service_injection_with_custom_name()!{
+	mut mod := set_up_module()
+
+	subject := mod.register[SomeServiceWithInjection]()
+
+	mod.inject() or { panic(err)}
+
+	mod.get[SimpleService](none)!.count = 42
+
+	assert subject.custom_name.count == 42
+}
+
+fn test_service_with_initialization()!{
+	mut mod := set_up_module()
+
+	mod.init()!
+
+	service := mod.get[SimpleService](none)!
+
+	assert service.count == 33
 }
