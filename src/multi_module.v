dba4a6600f5cc08b9f2f9d42a61fa703e032f61c
jaume <jaume.singlavalls@gmail.com>
2023-07-15 16:12:30 +0200

    adding a method use_instance to be able to use any instance as injectable

--- src/multi_module.v
+++ src/multi_module.v
@@ -28,3 +28,59 @@ pub fn (mut self Module) register_and_export[T]() &T {
 
 	return new_service
 }
+
+
+pub fn (mut self Module) use_instance[T](instance T, export bool) {
+	typ_idx := typeof[T]().idx
+	typ := reflection.get_type(typ_idx) or { panic('Type not found ${T.name}') }
+
+	if typ_idx in self.services {
+		panic('Type ${typ.name} has been already registered')
+	}
+
+	// Workarround to avoid a V bug
+	// ToDo: remove when https://github.com/vlang/v/issues/18294 is fixed
+	if false {
+		self.inject_to_object[T](mut new_service) or {panic(err)}
+	}
+
+	self.services[typ_idx] = Service{
+		name: T.name
+		typ: typ_idx
+		instance: instance
+		originalptr: instance
+		inject: fn ()!{}
+	}
+
+	if export {
+		self.export[T]()
+	}
+}
+/*
+pub fn (mut self Module) use_factory[F,T](factory fn (c F) T, export bool) {
+	typ_idx := typeof[T]().idx
+	typ := reflection.get_type(typ_idx) or { panic('Type not found ${T.name}') }
+
+	if typ_idx in self.services {
+		panic('Type ${typ.name} has been already registered')
+	}
+
+	// Workarround to avoid a V bug
+	// ToDo: remove when https://github.com/vlang/v/issues/18294 is fixed
+	if false {
+		self.inject_to_object[T](mut new_service) or {panic(err)}
+	}
+
+	self.services[typ_idx] = Service{
+		name: T.name
+		typ: typ_idx
+		instance: instance
+		originalptr: instance
+		inject: fn ()!{}
+	}
+
+	if export {
+		self.export[T]()
+	}
+}
+*/
